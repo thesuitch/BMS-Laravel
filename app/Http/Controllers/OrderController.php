7fd9bf977de086cd4e44db3c9ab231643fa8eccf
= <=>
2024-06-05 03:08:08 +0500

    mz : order list asc by order_date

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -149,7 +149,7 @@ class OrderController extends Controller
                 $columnName = $column_order[$orderColumnIndex];
                 $results = $results->orderBy($columnName, $orderDirection)->paginate($per_page);
             } else{
-                $results = $results->orderBy('b_q.order_date', 'ASC')->paginate($per_page);
+                $results = $results->orderBy('b_q.order_date', 'desc')->paginate($per_page);
             }
             
            return $results;
